da8c9e3e6fdc405eb724198d24942e4a6069c2bc
taisaburo_fujio <taisaburo.fujio@gmail.com>
2019-10-19 11:03:58 +0900

    モデル作成

---
+++ db/migrate/20191019015731_create_stations.rb
@@ -0,0 +1,12 @@
+class CreateStations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :stations do |t|
+      t.string :station_line
+      t.string :station_name
+      t.integer :walk_from
+      t.references :real_estates, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
