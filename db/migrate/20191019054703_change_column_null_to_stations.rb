1c7f705f7e51502bfc9e7d799acf908515871c35
taisaburo_fujio <taisaburo.fujio@gmail.com>
2019-10-19 14:51:56 +0900

    モデル非ナル制約修正

--- db/migrate/20191019054703_change_column_null_to_stations.rb
+++ db/migrate/20191019054703_change_column_null_to_stations.rb
@@ -1,10 +1,13 @@
 class ChangeColumnNullToStations < ActiveRecord::Migration[5.0]
-  def change
+  def up
+    change_column_null :stations, :station_line, null: true
+    change_column_null :stations, :station_name, null: true
+    change_column_null :stations, :walk_from, null: true
+  end
+
+  def down
     change_column_null :stations, :station_line, null: false
-    change_column :stations, :station_line, :string, default: "不明"
     change_column_null :stations, :station_name, null: false
-    change_column :stations, :station_name, :string, default: "不明"
     change_column_null :stations, :walk_from, null: false
-    change_column :stations, :walk_from, :integer, default: "不明"
   end
 end
